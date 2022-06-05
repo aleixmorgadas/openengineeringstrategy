31b406e63990d19a028b6608e5fdfa25f4774933
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2022-06-05 22:46:45 +0200

    debugging twitter og

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,14 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:url" content="https://openengineeringstrategy.com/" key="url" />
         <meta property="og:site_name" content="Open Engineering Strategy" key="site_name" />
         <meta property="og:image" content="https://openengineeringstrategy.com/og.png" key="image" />
+        <meta property="og:image:width" content="1200" key="image_width"/>
+        <meta property="og:image:height" content="630" key="image_height" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image:alt" content="Open Engineering Strategy" key="alt_image" />
-        <meta name="twitter:title"  content="Open Engineering Strategy" key="twitter_title" />
-        <meta name="twitter:description" content="Open Engineering Strategy Movement" key="twitter_description"/>
-        <meta name="twitter:image" content="https://openengineeringstrategy.com/og.png" key="twitter_image"/>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image:alt" content="Open Engineering Strategy" key="alt_image" />
+        <meta property="twitter:title"  content="Open Engineering Strategy" key="twitter_title" />
+        <meta property="twitter:description" content="Open Engineering Strategy Movement" key="twitter_description"/>
+        <meta property="twitter:image" content="https://openengineeringstrategy.com/og.png" key="twitter_image"/>
 
       </Head>
       <Component {...pageProps} />
