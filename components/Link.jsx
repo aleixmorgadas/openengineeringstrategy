5db6b2a153773d97b04ce090aae3c3562fe40e69
Aleix Morgadas <hello@aleixmorgadas.dev>
2022-09-04 11:46:27 +0200

    orange

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,6 +1,6 @@
 export default function Link(props) {
   return (
-    <span className="text-indigo-400 hover:cursor-pointer">
+    <span className="text-orange-400 hover:cursor-pointer">
       <a href={props.href} target="_blank" rel="noreferrer">{props.children}</a>
     </span>
   )
